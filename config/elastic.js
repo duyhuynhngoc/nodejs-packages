5d60e5813a1e2c1b1e668b468b5f2aeb631e6a3f
Ngoc Duy <duy.hngoc@gmail.com>
2015-09-28 18:57:34 +0700

    Duy Huynh

---
+++ config/elastic.js
@@ -0,0 +1,6 @@
+/**
+ * Code Owner: CCIntegration Inc. S.P.I.D.E.R framework
+ * Modified date: 9/28/2015
+ * Modified by: Duy Huynh
+ */
+
---
+++ modules/utilities/JsonReader.js
@@ -0,0 +1,12 @@
+/**
+ * Code Owner: CCIntegration Inc. S.P.I.D.E.R framework
+ * Modified date: 9/28/2015
+ * Modified by: Duy Huynh
+ */
+
+var JsonReader = module.exports = {},
+    fs = require('fs');
+
+JsonReader.parse = function(path){
+
+};
